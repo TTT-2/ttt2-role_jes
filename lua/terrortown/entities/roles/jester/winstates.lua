b86bdbc1f01ea2ebec64d0da07bde0873d978d4b
Histalek <16392835+Histalek@users.noreply.github.com>
2019-09-09 10:48:47 +0200

    avoidedRoles fix
    also updated to use non-deprecated function

--- lua/terrortown/entities/roles/jester/winstates.lua
+++ lua/terrortown/entities/roles/jester/winstates.lua
@@ -96,13 +96,13 @@ function JesterWinstateOne(ply, killer)
 	local rd = killer:GetSubRoleData()
 	local avoidedRoles = {}
 
-	for _, v in pairs(GetRoles()) do
+	for _, v in pairs(roles.GetList()) do
 		if v.defaultTeam == rd.defaultTeam then
 			avoidedRoles[v] = true
 		end
 	end
 
-	avoidedRoles[JESTER] = true
+	avoidedRoles[ROLE_JESTER] = true
 
 	JesterRevive(ply, function(p)
 		p:SelectRandomRole(avoidedRoles)
@@ -121,13 +121,13 @@ function JesterWinstateTwo(ply, killer)
 
 		local avoidedRoles = {}
 
-		for _, v in pairs(GetRoles()) do
+		for _, v in pairs(roles.GetList()) do
 			if v.defaultTeam == defaultTeam then
 				avoidedRoles[v] = true
 			end
 		end
 
-		avoidedRoles[JESTER] = true
+		avoidedRoles[ROLE_JESTER] = true
 
 		hook.Remove("PostPlayerDeath", "JesterWaitForKillerDeath_" .. ply:Nick())
 
@@ -181,13 +181,13 @@ function JesterWinstateFive(ply, killer)
 	local rd = killer:GetSubRoleData()
 	local avoidedRoles = {}
 
-	for _, v in pairs(GetRoles()) do
+	for _, v in pairs(roles.GetList()) do
 		if v.defaultTeam == rd.defaultTeam then
 			avoidedRoles[v] = true
 		end
 	end
 
-	avoidedRoles[JESTER] = true
+	avoidedRoles[ROLE_JESTER] = true
 
 	killer:Kill()
 	killer:ChatPrint("You were killed, because you killed the Jester!")
